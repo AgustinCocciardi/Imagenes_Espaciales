90e7ef70992088b86d8ae2f450dd7c58ca2fece5
AgustinCocciardi <42738793+AgustinCocciardi@users.noreply.github.com>
2019-05-12 10:31:17 -0300

    Comprimir imagen funciona

---
+++ Imágenes Espaciales/src/paquete/Compresor.java
@@ -0,0 +1,129 @@
+package paquete;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Compresor {
+	
+	private ImagenEspacial imagenDescomprimida;
+	private ImagenEspacial imagenComprimida;
+	
+	public Compresor(String imagenDescomprimida, String imagenComprimida) {
+		this.imagenDescomprimida = new ImagenEspacial(imagenDescomprimida);
+		this.imagenComprimida = new ImagenEspacial(imagenComprimida);
+	}
+	
+	public void setComprimir(String imagenComprimida) {
+		this.imagenDescomprimida.setColores(imagenComprimida);
+	}
+	
+	public void setDescomprimir(String imagenDescomprimida) {
+		this.imagenComprimida.setColores(imagenDescomprimida);
+	}
+	
+	public String getImagenDescomprimida() {
+		return imagenDescomprimida.getColores();
+	}
+
+	public String getImagenComprimida() {
+		return imagenComprimida.getColores();
+	}
+
+	public void resolver() {
+		this.comprimir();
+		this.descomprimir();
+	}
+	
+	private void comprimir() {
+		String imagen = this.imagenDescomprimida.getColores();
+		String nuevaImagen = "";
+		int cantidadRepeticiones = 0;
+		char letraActual = 0;
+		boolean letraAsignada = false;
+		int i = 0, longitud = imagen.length();
+		while(i<longitud) {
+			if(letraAsignada == false) {
+				cantidadRepeticiones = 1;
+				letraAsignada = true;
+				letraActual = imagen.charAt(i);
+			}
+			else {
+				if(imagen.charAt(i) == letraActual) {
+					cantidadRepeticiones++;
+				}
+				else {
+					i--;
+					letraAsignada = false;
+					if(cantidadRepeticiones > 4) {
+						nuevaImagen += "(" + letraActual + cantidadRepeticiones + ")";
+					}
+					else {
+						for(int j=0; j<cantidadRepeticiones; j++) {
+							nuevaImagen += letraActual;
+						}
+					}
+					
+				}
+				if(i+1 == longitud) {
+					if(cantidadRepeticiones > 4) {
+						nuevaImagen += "(" + letraActual + cantidadRepeticiones + ")";
+					}
+					else {
+						for(int j=0; j<cantidadRepeticiones; j++) {
+							nuevaImagen += letraActual;
+						}
+					}
+				}
+			}
+			i++;
+		}
+		this.setComprimir(nuevaImagen);
+	}
+	
+	private void descomprimir() {
+		String imagen = this.imagenComprimida.getColores();
+		String nuevaImagen = "";
+		int i = 0, longitud = imagen.length();
+		char letraActual = 0;
+		char letraRepetida = 0;
+		int repeticiones = 0;
+		while(i < longitud) {
+			letraActual = imagen.charAt(i);
+			if(letraActual == '(') {
+				repeticiones = 0;
+				letraRepetida = imagen.charAt(++i);
+				letraActual = imagen.charAt(++i);
+				while( letraActual != ')' ) {
+					repeticiones++;
+					letraActual = imagen.charAt(++i);
+				}
+				for (int j=0; j<repeticiones; j++) {
+					nuevaImagen += letraRepetida;
+				}
+			}
+			else {
+				nuevaImagen += letraActual;
+			}
+			i++;
+		}
+		this.setDescomprimir(nuevaImagen);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Scanner entrada = new Scanner(new FileReader("imagenes.in"));
+		String imagenDescomprimida = entrada.nextLine();
+		String imagenComprimida = entrada.nextLine();
+		Compresor compresor = new Compresor(imagenDescomprimida , imagenComprimida);
+		entrada.close();
+		compresor.resolver();
+		PrintWriter salida = new PrintWriter(new FileWriter("imagenes.out"));
+		salida.println(compresor.getImagenDescomprimida());
+		salida.println(compresor.getImagenComprimida());
+		salida.println();
+	}
+	
+}
---
+++ Imágenes Espaciales/src/paquete/ImagenEspacial.java
@@ -0,0 +1,19 @@
+package paquete;
+
+public class ImagenEspacial {
+	
+	private String colores;
+	
+	public ImagenEspacial(String imagen) {
+		this.colores = imagen;
+	}
+	
+	public String getColores() {
+		return this.colores;
+	}
+	
+	public void setColores(String colores) {
+		this.colores = colores;
+	}
+	
+}
